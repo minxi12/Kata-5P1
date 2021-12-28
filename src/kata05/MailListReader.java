3073a53d2465175f228aa8dc6b7215e7736130ff
minxi12 <91190199+minxi12@users.noreply.github.com>
2021-12-28 00:01:07 +0000

    Update MailListReader.java

--- src/kata05/MailListReader.java
+++ src/kata05/MailListReader.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package kata5p1;
+package kata05;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
